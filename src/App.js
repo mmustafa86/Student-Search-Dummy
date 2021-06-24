4bd7ffb2bf2fc2f73ac86d6541e3a5b5ff76fdad
mohammed mustafa <mohammed.alkhddar@gmail.com>
2021-06-23 23:15:15 -0500

    finished first part

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React ,{Fragment}from 'react'
 import Home from './components/pages/Home'
 import StudentsState from './context/StudentsState'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
--- src/components/student/Student.js
+++ src/components/student/Student.js
@@ -1,4 +1,5 @@
-import React ,{useContext,useEffect}from 'react';
+import React ,{useContext,useEffect ,Fragment}from 'react';
+import {Card} from "react-bootstrap";
 import StudentsContext from '../../context/studentsContext'
 
 
@@ -10,18 +11,38 @@ const {getStuents,students}=sudentsContext;
 
 useEffect(() => {
     getStuents()
-}, [])
+}, []);
 
-console.log(students);
-
+const average=(array) =>{
+    let sum= array.reduce((a, b) => a + b) ;
+    let avg=sum/ array.length
+    return avg;
+};
 
     return (
-        <div>
-           Student 
-        </div>
+    
+        <Fragment>
+     {students.students!==undefined&& students.students.map(student => (
+        <Card style={{ width: '18rem' }} key={student.id}>
+        <Card.Img variant="top" src={student.pic} style={{ width: '7rem' }}/>
+        <Card.Body>
+          <Card.Title>{student.firstName} {student.lastName}</Card.Title>
+          <Card.Text>
+           <p>Email: {student.email}</p>
+           <p>Company: {student.company}</p>
+           <p>Skill: {student.skill}</p>
+           <p>Average: {average(student.grades)}%</p>
+          </Card.Text>
+   
+        </Card.Body>
+      </Card>
+     ))}
+            
+    
+  
+      </Fragment>
     )
 }
-
 
 
 export default Student;
